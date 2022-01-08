9ac541d510066f93a0c72b6d08803843f044b020
rami <rami.develop@gmail.com>
2022-01-08 14:23:37 -0300

    Added file index.html to render on '/' url

--- myApp.js
+++ myApp.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+
 console.log("Hello World");
 
 app.get('/', function(req, res) {
-	
-	res.send('Hello Express');
+	let path = `${__dirname}/views/index.html`;
+	res.sendFile(path);
 });
 
 
