54e9946e1647747fc80af7a1029bd85de7d8054a
ramidevictoria <>
2022-01-09 14:37:18 +0000

    Added time request over /nnow

--- myApp.js
+++ myApp.js
@@ -21,6 +21,13 @@ str = process.env.MESSAGE_STYLE === 'uppercase' ? str.toUpperCase() : str;
   res.json({"message": str});
 });
 
+app.get('/now', function(req,res,next) {
+  req.time = new Date().toString();
+  next();
+}, function(req,res) {
+  res.send({time: req.time});
+});
+
 
 
 
